1e3982db53c6b1affe4e6f34ffedf0573acd918c
Tom McCarthy <65662436+tommcc314@users.noreply.github.com>
2020-08-27 11:06:07 -0400

    Create Calculation.java

---
+++ Lab4/Calculation.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4;
+
+public class Calculation {
+    private double result;
+    public Calculation(double n1, double n2, String operation) {
+        switch (operation) {
+            case "+":
+                result = n1 + n2;
+                break;
+            case "-":
+                result = n1 - n2;
+                break;
+            case "*":
+                result = n1 * n2;
+                break;
+            case "/":
+                result = n1 / n2;
+                break;
+        }
+    }
+    public double getResult() {
+        return result;
+    }
+}
